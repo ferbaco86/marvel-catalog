16832e3d6846fc18d3fdc347f828aaa431a88c06
Fernando Bahamondes <ferbaco@gmail.com>
2021-02-19 23:59:32 -0300

    Add linters config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test Text
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
