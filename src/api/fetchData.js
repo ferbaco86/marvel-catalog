7b5e9964656adf03490a78601fd556c55914594a
ferbaco86 <fernando.bahamondes.correa@gmail.com>
2024-09-04 00:40:38 -0400

    update dependencies

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -9,7 +9,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const fetchData = () => dispatch => {
   const { offset } = store.getState().offset;
-  const apiUrl = `https://gateway.marvel.com:443/v1/public/characters?orderBy=name&limit=100&offset=${offset}&apikey=${API_KEY}`;
+  const apiUrl = `https://gateway.marvel.com/v1/public/characters?orderBy=name&limit=100&offset=${offset}&apikey=${API_KEY}`;
   dispatch(fetchDataPending());
   fetch(apiUrl, config)
     .then(response => response.json())
